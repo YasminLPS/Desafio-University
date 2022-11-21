deee4d9f8fffaff46a4801decca25d6b33cdae5c
Yasmin Lopes <yasminqalopes54@gmail.com>
2022-11-21 08:52:17 -0300

    Ajustado rotas e modal

--- database.js
+++ database.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql2');
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    database: 'university',
-    user: 'root',
-    password: '24153100'
+	host: process.env.MYSQL_HOST,
+	database: process.env.MYSQL_DATABASE,
+	port: process.env.MYSQL_PORT,
+	user: process.env.MYSQL_USER,
+	password: process.env.MYSQL_PASSWORD,
 });
-
 connection.connect(function(error){
     if(error){
         throw error;
     }
     else{
-        console.log('MySQL database is connected Succesfully');
+        console.log("MySQL database is connected on port " + process.env.MYSQL_PORT);
     }
 });
 
--- routes/alunos_data.js
+++ routes/alunos_data.js
@@ -1,94 +1,89 @@
-var express = require('express');
+var express = require("express");
 
 var router = express.Router();
 
-var database = require('../database');
+var database = require("../database");
 
-router.get("/", function(request, response, next ){
-    response.render('alunos_data');
+router.get("/", function (req, res, next) {
+	res.render("alunos_data");
 });
 
-router.get("/action", function(request, response, next ){
-    var query = "SELECT * FROM alunos ORDER BY id";
-        
-    database.query(query, function(error, data){
-        response.json({
-            data:data
-        });
-    });  
+router.get("/alunos", function (req, res, next) {
+	var query =
+		"SELECT alunos.id,alunos.nome, alunos.bday, cursos.nome as cursoId, semestres.nome as semestreId, matricula FROM alunos";
+	query += " INNER JOIN cursos ON cursos.id = alunos.cursoId";
+	query += " INNER JOIN semestres ON semestres.id = alunos.semestreId";
+
+	database.query(query, function (error, data) {
+		if (error) throw error;
+		res.json({
+			data: data,
+		});
+	});
 });
 
-router.post("/action", function(request, response, next){
- const {nome,bday,cursos,semestre, matricula} = request.body
+router.post("/alunos", function (req, res, next) {
+	const { nome, bday, cursos, semestre, matricula } = req.body;
 
-        var query = `
+	var query = `
         INSERT INTO alunos
         (nome, bday, matricula, cursoId, semestreId)
         VALUES ("${nome}","${bday}","${matricula}","${cursos}","${semestre}")
         `;
 
-        database.query(query, function(error,data){
-            if(error) throw error
-            response.json({
-                message: 'Aluno cadastrado com sucesso'
-            });
-        });
+	database.query(query, function (error, data) {
+		if (error) throw error;
+		res.json({
+			message: "Aluno cadastrado com sucesso",
+		});
+	});
 });
 
-router.get("/action", function(request, response, next){
-    var id = request.body.id;    
-    var query = `
+router.get("/alunos/:id", function (req, res, next) {
+	var query = `
         SELECT * FROM alunos
-        WHERE id= "${id}"
+        WHERE id= "${req.params.id}"
         `;
 
-    database.query(query, function(error,data){
-        if(error) throw error
-        response.json(data[0]);
-    });
+	database.query(query, function (error, data) {
+		if (error) throw error;
+		res.json(data[0]);
+	});
 });
 
-router.put("/action", function(request, response, next){
-    
-    const {id,nome,bday,cursos,semestre, matricula} = request.body
+router.put("/alunos/:id", function (req, res, next) {
+	const { nome, bday, cursos, semestre, matricula } = req.body;
 
-    var query = `
+	var query = `
     UPDATE alunos
     SET nome = "${nome}",
     bday = "${bday}",
     cursoId = "${cursos}",
     semestreId = "${semestre}",
     matricula = "${matricula}"
-    WHERE id = "${id}"
+    WHERE id = "${req.params.id}"
     `;
 
-    database.query(query, function(error, data){
-        console.log(query)
-        if(error){
-        }
-        response.json({
-            message: 'Aluno editado com sucesso!'
-        });
-    });
-   
+	database.query(query, function (error, data) {
+		if (error) throw error;
+		res.json({
+			message: "Aluno editado com sucesso!",
+		});
+	});
 });
 
-router.delete("/action", function(request, response, next){
-    var id = request.body.id;
-    var query = `
+router.delete("/alunos/:id", function (req, res, next) {
+	var query = `
     DELETE from alunos
-    WHERE id = "${id}"
+    WHERE id = "${req.params.id}"
     `;
 
-    database.query(query, function(error, data){
-        if(error) throw error
-        response.json({
-            message: 'Aluno exluido com sucesso!'
-        });
-    });
-   
+	database.query(query, function (error, data) {
+		if (error) throw error;
+		res.json({
+			message: "Aluno exluido com sucesso!",
+		});
+	});
 });
-
-
 
 module.exports = router;
